8883feb7b712f59131c7c91a5c351d836b448c29
busrasariguzel <busra.sariguzel@codeimmersives.com>
2020-04-09 12:01:02 -0400

    initial

---
+++ src/components/Animals.js
@@ -0,0 +1,110 @@
+import React, {Component} from 'react'
+
+let animals = [
+{
+
+    name : 'Zeyno',
+    type: 'Dog',
+    description: faker.lorem.sentence(),
+    image:'',
+
+},
+{
+    name :'Betty',
+    type: 'dog',
+    description: faker.lorem.sentence(),
+    image:'/images/dog1.jpg',
+
+},
+{
+    name : 'Kamuran',
+    type: 'dog',
+    description: faker.lorem.sentence(),
+    image:'/images/dog5.jpg',
+
+},
+{
+    name : 'Loco',
+    type: 'dog',
+    description: faker.lorem.sentence(),
+    image:'/images/kopek2.jpg',
+
+},
+{
+    name : 'Sally',
+    type: 'lion',
+    description: faker.lorem.sentence(),
+    image:'/images/lion1.jpg'
+
+},
+{
+    name : 'Pamuk',
+    type: 'lion',
+    description: faker.lorem.sentence(),
+    image:'/images/lion2.jpg',
+
+},
+
+{
+    name : 'Muzur',
+    type: 'lion',
+    description: faker.lorem.sentence(),
+    image:'/images/lion5.jpg',
+
+},
+{
+    name : 'Beth',
+    type: 'lion',
+    description: faker.lorem.sentence(),
+    image:'/images/lion6.jpg',
+
+},
+{
+    name : 'Kuzu',
+    type: 'sheep',
+    description: faker.lorem.sentence(),
+    image:'/images/kuzu2.jpg',
+
+},
+{
+    name : 'Toddy',
+    type: 'sheep',
+    description: faker.lorem.sentence(),
+    image:'/images/kuzu3.jpg',
+
+},
+{
+    name : 'Bidik',
+    type: 'sheep',
+    description: faker.lorem.sentence(),
+    image:'/images/kuzu4.jpg',
+
+},
+{
+    name : 'Bodo',
+    type: 'sheep',
+    description: faker.lorem.sentence(),
+    image:'/images/kuzu11.jpg',
+
+},
+
+]
+
+
+
+
+
+
+class Animal extends Component {
+constructor(){
+super()
+this.state = {
+
+}
+}
+
+
+
+}
+
+export default Animal
---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Animal from './Animals'
+const App = () => {
+return (
+<div>
+
+    <Animal></Animal>
+</div>
+
+)
+}
+
+
+export default App
